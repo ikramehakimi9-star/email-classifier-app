a3d772e35712a7b2ef68a850946038b293b3e7fe
ikramehakimi9-star <ikramehakimi9@gmail.com>
2026-06-15 23:49:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,11 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Téléchargement des dépendances NLTK obligatoires
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 ps = PorterStemmer()
-
-# OPTIMISATION CRUCIALE : Charger les stopwords une seule fois f l-début
 stop_words_set = set(stopwords.words('english'))
 
 def transform_text(text):
@@ -28,7 +25,6 @@ def transform_text(text):
     y.clear()
 
     for i in text:
-        # Utilisation du set optimisé
         if i not in stop_words_set and i not in string.punctuation:
             y.append(i)
 
@@ -40,7 +36,6 @@ def transform_text(text):
 
     return " ".join(y)
 
-# Chargement des fichiers de votre modèle
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -49,76 +44,11 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    if input_sms: # Évite de lancer le calcul si la case est vide
-        # 1. preprocess
+    if input_sms:
         transformed_sms = transform_text(input_sms)
-        # 2. vectorize
         vector_input = tfidf.transform([transformed_sms])
-        # 3. predict
         result = model.predict(vector_input)[0]
-        # 4. Display
-        if result == 1:
-            st.header("Spam 🚨")
-        else:
-            st.header("Not Spam ✅")import streamlit as st
-import pickle
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-
-# Téléchargement des dépendances NLTK obligatoires
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('stopwords')
-
-ps = PorterStemmer()
-
-# OPTIMISATION CRUCIALE : Charger les stopwords une seule fois f l-début
-stop_words_set = set(stopwords.words('english'))
-
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        # Utilisation du set optimisé
-        if i not in stop_words_set and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-# Chargement des fichiers de votre modèle
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
-st.title("Email/SMS Spam Classifier")
-
-input_sms = st.text_area("Enter the message")
-
-if st.button('Predict'):
-    if input_sms: # Évite de lancer le calcul si la case est vide
-        # 1. preprocess
-        transformed_sms = transform_text(input_sms)
-        # 2. vectorize
-        vector_input = tfidf.transform([transformed_sms])
-        # 3. predict
-        result = model.predict(vector_input)[0]
-        # 4. Display
+        
         if result == 1:
             st.header("Spam 🚨")
         else:
